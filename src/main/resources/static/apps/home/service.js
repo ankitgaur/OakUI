74a0685e3ecf9ef13b55a37d3469231e71f9ee00
ankitgaur <ankitgaur@Ankits-MacBook-Pro.local>
2017-02-20 06:45:01 -0700

    removed unnecessary methods.

--- src/main/resources/static/apps/home/service.js
+++ src/main/resources/static/apps/home/service.js
@@ -28,219 +28,6 @@ homeApp
 						  
 							
 						    };
-							
-							oakHomeFactory.createForumPosts = function(
-									ForumPostData) {
-								var url = AppConfig.appUrl + 'forum_post';
-								var req = {
-									method : 'POST',
-									url : url,
-									data : ForumPostData,
-									headers : {
-										Authorization : 'Basic '
-												+ AppConfig.key
-									}
-								}
-
-								return $http(req)
-										.then(
-												function success(response) {
-													$log
-															.debug('article created successfully ');
-												},
-												function error(response) {
-													$log
-															.debug('There is some issue while getting data from rest service');
-												});
-							}
-							
-
-							oakHomeFactory.createTopic = function(
-									forumTopicData) {
-								var url = AppConfig.appUrl + 'forum_topics';
-								return $http
-										.post(
-												url,
-												forumTopicData,
-												{
-													transformRequest : angular.identity,
-													headers : {
-														Authorization : 'Basic '
-																+ AppConfig.key,
-														'Content-Type' : undefined
-													}
-
-												})
-										.then(
-												function success(response) {
-													return response.data;
-													showSuccessAlert('Forum Topic was created.');
-												},
-												function error(response) {
-													showErrorAlert('There was some issue while creating Forum Topic. Please contact your System Administrator.');
-												});
-							}
-
-							oakHomeFactory.createBlog = function(bdata) {
-								var url = AppConfig.appUrl + 'blogs';
-
-								$http
-										.post(
-												url,
-												bdata,
-												{
-													transformRequest : angular.identity,
-													headers : {
-														Authorization : 'Basic '
-																+ AppConfig.key,
-														'Content-Type' : undefined
-													}
-
-												})
-
-										.success(
-												function() {
-													showSuccessAlert("Blog was created successfully.");
-												})
-
-										.error(
-												function() {
-													showErrorAlert('There were some issues while creating your blog. Please contact your System Administrator');
-												});
-
-							};
-
-							oakHomeFactory.createBlogPost = function(bdata) {
-								var url = AppConfig.appUrl + 'blog_entries';
-								$http
-										.post(
-												url,
-												bdata,
-												{
-													transformRequest : angular.identity,
-													headers : {
-														Authorization : 'Basic '
-																+ AppConfig.key,
-														'Content-Type' : undefined
-													}
-												})
-
-										.success(
-												function() {
-													showSuccessAlert("Blog Post was created successfully.");
-												})
-
-										.error(
-												function() {
-													showErrorAlert('There were some issues while creating your blog post. Please contact your System Administrator');
-												});
-							};
-
-							oakHomeFactory.getBlogs = function() {
-								var url = AppConfig.appUrl + 'blogs';
-
-								return $http({
-									method : 'GET',
-									url : url,
-									crossDomain : true
-								})
-										.then(
-												function successCallback(
-														response) {
-													return response.data;
-												},
-												function errorCallback(response) {
-													$log
-															.debug('There is some issue while getting blog category from rest service');
-												});
-
-							};
-
-							oakHomeFactory.getMostPopularBlogsPost = function() {
-								var url = AppConfig.appUrl
-										+ 'popular_blog_entries';
-								return $http({
-									method : 'GET',
-									url : url,
-									crossDomain : true
-								})
-										.then(
-												function successCallback(
-														response) {
-													return response.data;
-												},
-												function errorCallback(response) {
-													$log
-															.debug('There is some issue while getting data from rest service');
-												});
-
-							}
-
-							oakHomeFactory.getMyBlogs = function() {
-								var url = AppConfig.appUrl + '/myblogs';
-
-								return $http(
-										{
-											method : 'GET',
-											url : url,
-											crossDomain : true,
-											headers : {
-												Authorization : 'Basic '
-														+ AppConfig.key
-											}
-										})
-										.then(
-												function successCallback(
-														response) {
-													return response.data;
-												},
-												function errorCallback(response) {
-													$log
-															.debug('There is some issue while getting data from rest service');
-												});
-
-							};
-
-							oakHomeFactory.getMyBlogPosts = function() {
-								var url = AppConfig.appUrl
-										+ 'blog_entries/user';
-
-								return $http({
-									method : 'GET',
-									url : url,
-									crossDomain : true
-								})
-										.then(
-												function successCallback(
-														response) {
-													return response.data;
-												},
-												function errorCallback(response) {
-													$log
-															.debug('There is some issue while getting data from rest service');
-												});
-
-							};
-
-							oakHomeFactory.getTopBlogs = function() {
-								var url = AppConfig.appUrl + 'blog_entries';
-
-								return $http({
-									method : 'GET',
-									url : url,
-									crossDomain : true
-								})
-										.then(
-												function successCallback(
-														response) {
-													return response.data;
-												},
-												function errorCallback(response) {
-													$log
-															.debug('There is some issue while getting data from rest service');
-												});
-
-							};
 
 							oakHomeFactory.getPopularContent = function(content_type,tags) {
 								var url = AppConfig.appUrl
@@ -267,6 +54,28 @@ homeApp
 							oakHomeFactory.getContent = function(content_type,tags) {
 								var url = AppConfig.appUrl
 								+ 'content/type/' + content_type + '?tags='
+								+ tags;
+
+								return $http({
+									method : 'GET',
+									url : url,
+									crossDomain : true
+								})
+										.then(
+												function successCallback(
+														response) {
+													return response.data;
+												},
+												function errorCallback(response) {
+													$log
+															.debug('There is some issue while getting data from rest service for category ');
+												});
+
+							};
+							
+							oakHomeFactory.getContentForParent = function(content_type,parentid) {
+								var url = AppConfig.appUrl
+								+ 'content/parent/' + content_type + '/'+ parentid
 								+ tags;
 
 								return $http({
